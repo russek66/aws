79687d4ed21062eee4f55370af2899db44088357
russEk66 <russEk66@gmail.com>
2023-10-31 17:40:23 +0100

    added basic Request handling, more to come

---
+++ src/Core/Request.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Core;
+
+class Request
+{
+
+    public static function get(mixed $key): mixed{
+        if (isset($_GET[$key])) {
+            return $_GET[$key];
+        }
+    }
+}
