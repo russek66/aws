e0929b264c95bb67e44a04da337c9e446955f93a
russEk66 <russEk66@gmail.com>
2023-12-15 19:00:41 +0100

    reworked render JSON will be moved out in feature to handle possible code extensions

--- src/Core/View/View.php
+++ src/Core/View/View.php
@@ -35,10 +35,26 @@ class View
         require $filenamePath .'.php';
     }
 
-    public function renderJSON(mixed $data): void
+    public function renderJSON($code = 200, $data = null): string
     {
-        header("Content-Type: application/json");
-        json_encode($data);
+        header_remove();
+        http_response_code($code);
+        header("Cache-Control: no-transform,public,max-age=300,s-max-age=900");
+        header('Content-Type: application/json');
+        $status = array(
+            200 => '200 OK',
+            400 => '400 Bad Request',
+            422 => 'Unprocessable Entity',
+            500 => '500 Internal Server Error'
+        );
+
+        header('Status: '.$status[$code]);
+
+        return json_encode([
+            'status' => $code < 300, // success or not?
+            'data' => $data
+        ]);
+
     }
 
 }
