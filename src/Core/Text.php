49bb9ce16051342738e302cdc619438367a82939
russEk66 <russEk66@gmail.com>
2024-01-03 02:10:19 +0100

    Added Text trait main usage -> return feedback from validation to save it in logs / return feedback to enduser

---
+++ src/Core/Text.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Core;
+
+trait Text
+{
+
+    use Config {
+        Config::get as getConfig;
+    }
+
+    private mixed $texts = null;
+
+    public function get(string $file, string $key)
+    {
+
+        if (! $this->texts) {
+            $this->texts = require($this->getConfig('TEXTS_PATH') . ucwords($file) . 'Texts.php');
+        }
+        if (! array_key_exists($key, $this->texts)) {
+            return null;
+        }
+
+        return $this->texts[$key];
+    }
+}
